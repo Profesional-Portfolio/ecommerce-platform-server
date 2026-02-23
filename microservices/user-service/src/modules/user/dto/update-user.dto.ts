e5ee0f82a785688a53a941eba724ae25beca2628
Deivid182 <67056846+Deivid182@users.noreply.github.com>
2026-02-22 19:08:34 -0600

    Remove microservice folders before adding submodules

--- microservices/user-service/src/modules/user/dto/update-user.dto.ts
+++
@@ -1,9 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { IsOptional, IsBoolean } from 'class-validator';
-import { CreateUserDto } from './create-user.dto';
-
-export class UpdateUserDto extends PartialType(CreateUserDto) {
-  @IsOptional()
-  @IsBoolean({ message: 'isActive debe ser un valor booleano' })
-  isActive?: boolean;
-}
